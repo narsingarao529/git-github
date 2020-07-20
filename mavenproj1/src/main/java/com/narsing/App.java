65c99c1ef77869df37f52f9fd032585f63c87bfc
EC2 Default User <ec2-user@ip-172-31-42-141.ap-south-1.compute.internal>
2020-07-20 06:23:36 +0000

    inital maven project

---
+++ mavenproj1/src/main/java/com/narsing/App.java
@@ -0,0 +1,13 @@
+package com.narsing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
